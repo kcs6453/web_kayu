34bef306fcde99d403a51e1ee04ed7ad2f56e236
Keyrichio Sentana <147927942+kcs6453@users.noreply.github.com>
2024-03-19 13:32:30 +0700

    Add files via upload

---
+++ web_e-commerce/html/landing_page.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drink Shop</title>
+    <link rel="stylesheet" href="../css/landing_page.css">
+</head>
+
+<body>
+
+    <div class="nav">
+        <div class="kiri">
+            <img src="../image/_Pngtree_restaurant_drink_logo_design_template_3605892-removebg-preview.png" alt="">
+            <a href="#home"><h2>About Me</h2></a>
+            <a href="#about"><h2>Home</h2></a>
+        </div>
+        
+        <div class="kanan">
+            <a href="#testimony"><h2>Testimony</h2></a>
+            <a href="#platform"><h2>Platfrom</h2></a>
+            <a href="#berlangganan"><h2>Berlangganan</h2></a>
+        </div>
+    </div>
+
+    <div class="hero">
+        <div class="text-kiri">
+            <h2 id="nama-toko">Toko Minuman</h2>
+            <h1>Menjual Segala Jenis Minuman </h1>
+            <br>
+            <h2>Memastikan bahwa anda merasa terpuaskan dengan minuman yang kami produksi</h2>
+            <div class="button-toko">
+                <h3>Lihat Product</h3>
+            </div>
+        </div>
+
+        <div class="hero-img">
+            <img src="../image/_Pngtree_restaurant_drink_logo_design_template_3605892-removebg-preview.png" alt="">
+        </div>
+    </div>
+
+    <div id="home">
+        <img src="../image/WhatsApp Image 2024-03-18 at 00.10.27_60004ecb.jpg" alt="">
+
+        <div class="biodata">
+            <h3><span>Nama</span>  : Keyrichio Cleoz Sentana</h3>
+            <h3><span>TTL</span>   : Jakarta, 20 September 2007</h3>
+            <h3><span>Kelas</span> : XI-RPL2</h3>
+            <h3><span>Tugas</span> : PWPB</h3>
+        </div>
+    </div>
+
+    <div id="about">
+        <div class="text-about">
+            <h3>Toko Minuman</h3>
+            <h2>Minuman yang <span>sehat</span> dan <span>meningkatkan mood</span> anda</h2>
+            <br>
+            <p>Minuman yang kami produksi adalah minuman yang sehat dengan berbagai macam rempah-rempah dan tentunya akan meningkatkan mood anda karena beragam cita rasa yang ada pada minuman kami, Dan visi kami adalah membuat perut anda tetap sehat dan memanjakan lidah kalian.</p>
+        </div>
+
+        <div class="checkbox-group">
+            <h2>Keunggulan Product Kami</h2>
+
+            <div class="checkbox">
+                <img src="../image/image.png" alt="">
+                <p>Kami memastikan anda meminum minuman yang sehat agar kesehatan anda tetap terjaga</p>
+            </div>
+
+            <div class="checkbox">
+                <img src="../image/image.png" alt="">
+                <p>Kami membuat minuman yang beragam cita rasa tanpa mengurasi esensi kesehatan minuman kami</p>
+            </div>
+
+            <div class="checkbox">
+                <img src="../image/image.png" alt="">
+                <p>Kami menggunakan kemasan yang ramah lingkungan sehingga meminimalisir penambahan limbah di dunia</p>
+            </div>
+
+            <div class="checkbox">
+                <img src="../image/image.png" alt="">
+                <p>Kami melakukan quality control pada setiap minuman yang kami produksi sehingga minuman yang ada pada konsumen adalah minuman yang layak</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="testimony">
+        <div class="text-testimony">
+            <h2>Disamping ini adalah pendapat <span>pelanggan setia</span> kami</h2>
+            <p>Tentunya banyak konsumen yang berlangganan dan menjadi pelanggan setia dalam toko minuman ini di berbagai daerah tapi kami memiliki 1 pelanggan setia yang sangat kami apresiasi karena kesetiannya terhadap product kami</p>
+        </div>
+
+        <img src="../image/120374854_115531343642521_7656074376949973611_n.jpg" alt="">
+    </div>
+
+    <div id="platform">
+        <div class="text-platform">
+            <h3>Platform Kami</h3>
+            <h2><span>Dimanapun</span> anda,<br> <span>Jam</span> apapun, <span>Suasana</span> apapun <br><span>Kami ada</span></h2>
+            <p>Toko ini mempunyai banyak platform yang anda dapat akses di berbagi media, tentunya ini akan sangat memudahkan anda untuk melihat update dan berita-berita penting seputar toko ini, Kami berikan yang terbaik untuk anda</p>
+        </div>
+
+        <div class="platform-image-group">
+            <div class="platform-image">
+                <img src="../image/logo_sosmed/facebook-messenger.png" alt="">
+            </div>
+
+            <div class="platform-image">
+                <img src="../image/logo_sosmed/facebook.png" alt="">
+            </div>
+            
+            <div class="platform-image">
+                <img src="../image/logo_sosmed/github.png" alt="">
+            </div>
+            
+            <div class="platform-image">
+                <img src="../image/logo_sosmed/instagram.png" alt="" srcset="">
+            </div>
+            
+            <div class="platform-image">
+                <img src="../image/logo_sosmed/telegram.png" alt="">
+            </div>
+            
+            <div class="platform-image">
+                <img src="../image/logo_sosmed/tik-tok.png" alt="" srcset="">
+            </div>
+            
+            <div class="platform-image">
+                <img src="../image/logo_sosmed/twitter-alt.png" alt="">
+            </div>
+            
+            <div class="platform-image">
+                <img src="../image/logo_sosmed/whatsapp.png" alt="">
+            </div>
+            
+            <div class="platform-image">
+                <img src="../image/logo_sosmed/youtube.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div id="berlangganan">
+        <h3>Berlangganan</h3>
+        <h2>Mau Berlangganan? Berikut <span>Benefitnya</span></h2>
+        
+        <div class="benefit-group">
+            <div class="benefit">
+                <img src="../image/icon_benefit/membership-vip.png" alt="">
+                <p>Anda akan diberikan kartu VIP yang dapat digunakan sesuai kebutuhan</p>
+            </div>
+            
+            <div class="benefit">
+                <img src="../image/icon_benefit/air-conditioner.png" alt="">
+                <p>Anda akan mendapatkan tempat special yang pastinya nyaman untuk bersantai</p>
+            </div>
+            
+            <div class="benefit">
+                <img src="../image/icon_benefit/badge-percent (1).png" alt="">
+                <p>Discount yang tersedia setiap pembelian product dan dapat diupgrade</p>
+            </div>
+            
+            <div class="benefit">
+                <img src="../image/icon_benefit/bowl-soft-serve.png" alt="">
+                <p>Pelayanan terbaik akan dijalankan kepada pemilik kartu VIP</p>
+            </div>
+            
+            <div class="benefit">
+                <img src="../image/icon_benefit/face-thinking.png" alt="">
+                <p>Mau request campuran minuman? Pemilik kartu VIP boleh melakukan hal tersebut</p>
+            </div>
+            
+            <div class="benefit">
+                <img src="../image/icon_benefit/queue-signal.png" alt="">
+                <p>Pemilik kartu VIP tidak perlu mengantri untuk membeli product kami</p>
+            </div>
+            
+            <div class="benefit">
+                <img src="../image/icon_benefit/ticket-alt.png" alt="">
+                <p>Ticket Spesial akan diberikan kepada VIP untuk mengklaim 1 minuman</p>
+            </div>
+            
+            <div class="benefit">
+                <img src="../image/icon_benefit/user-chef.png" alt="">
+                <p>Pembeli VIP akan dilayani oleh bartender top yang memiliki predikat terbaik</p>
+            </div>
+            
+            <div class="benefit">
+                <img src="../image/icon_benefit/wifi.png" alt="">
+                <p>Pemilik kartu VIP diberikan Password wifi sehingga dapat menggunakan WiFi</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="footer">
+        <img src="../image/_Pngtree_restaurant_drink_logo_design_template_3605892-removebg-preview.png" alt="">
+
+        <div class="footer-text">
+            <h3>Biodata</h3>
+            <p>Keyrichio</p>
+            <p>16 tahun</p>
+            <p>Jakarta 20 September</p>
+            <p>4 Bersaudara</p>
+        </div>
+
+        <div class="footer-text">
+            <h3>Sekolah</h3>
+            <p>SMKN1 Binjai</p>
+            <p>Rekayasa Perangkat Lunak</p>
+            <p>XI-RPL2</p>
+            <p>Jalan Samanhudi, Tanah Merah</p>
+        </div>
+
+        <div class="footer-text">
+            <h3>Mapel</h3>
+            <p>PWPB</p>
+            <p>Design Web</p>
+            <p>Hypertext Markup Language</p>
+            <p>Cassading Style Sheet</p>
+            <p>Hypertext Preprocessor</p>
+        </div>
+
+        <div class="footer-text">
+            <h3>Sosmed</h3>
+            <p>keyrichio_sen</p>
+            <p>Keyrichio Cleoz Sentana</p>
+            <p>Key Leo Sen</p>
+            <p>Keyrichio Sentana</p>
+        </div>
+    </div>
+    
+    <div class="copyright">
+        <h3>Copyright &copy;2024, Designed By Keyrichio</h3>
+        
+
+        <div class="group-tombol">
+            <a href="https://github.com/kcs6453">
+                <div class="tombol">
+                    <img src="../image/logo_sosmed/github.png" alt="">
+                </div>
+            </a>
+
+            <a href="https:wa.me/+6285762241183">
+                <div class="tombol">
+                    <img src="../image/logo_sosmed/whatsapp.png" alt="">
+                </div>
+            </a>
+
+            <a href="https://www.instagram.com/keyrichio_sen/">
+                <div class="tombol">
+                    <img src="../image/logo_sosmed/instagram.png" alt="">
+                </div>
+            </a>
+        </div>
+    </div>
+
+</body>
+
+</html>
